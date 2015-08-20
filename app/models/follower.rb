8881042011455831455e2c711cb46f05639b7c7e
David <mail@daviduli.com>
2015-08-20 14:58:06 -0700

    Relationship Model

--- app/models/follower.rb
+++
@@ -1,8 +0,0 @@
-###########################################
-### Follower Model
-###########################################
-
-class Follower < ActiveRecord::Base
-  # belongs_to :user
-  # belongs_to :follower, :class_name => 'User'
-end

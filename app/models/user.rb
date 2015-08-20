2762ded1aaa42175701a2b869eeb2ade7ebe63e2
David <mail@daviduli.com>
2015-08-20 12:47:33 -0700

    DB Models & Migrations Complete Without Follower Capability

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@
 class User < ActiveRecord::Base
   # has_many :followers, class_name => 'Follower', :foreign_key => 'user_id'
   # has_many :following, class_name => 'Follower', :foreign_key => 'follower_id'
+  has_many :tweets
 end
---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+david = User.create(name: 'David Uli', email: 'mail@daviduli.com', handle: '#NorCalDavid', password_hash: ["password_hash", "$2a$10$9dNZbib6VQTXA7xPvoewMOnwHpPZer0ZnSxDQqcKdJx1sk6Pz.gA."])
+
+james = User.create(name: 'James Bomotti', email: 'test@thisserver.com', handle: '#JBomotti', password_hash: ["password_hash", "$2a$10$S0fnTfu3cLc1G7V5dMI/8uAzeLnxM5I5diiKT4T3EUMnAKpUcJICK"])
+
+Tweet.create( message: 'lilTwitter Rocks!', user_id: 2 )
+
+10.times do
+  Tweet.create( message: Faker::Lorem.characters(rand(105)+25), user_id: rand(2)+1 )
+end
+

f1ffcf9a9a45b7ef4f22b26eefccbcab13ea4ba3
David <mail@daviduli.com>
2015-08-20 12:04:22 -0700

    Update Migrations

--- db/migrate/20150820110414_create_users.rb
+++ db/migrate/20150820110414_create_users.rb
@@ -9,8 +9,8 @@ class CreateUsers < ActiveRecord::Migration
       t.string :email
       t.string :handle
       t.string :password_hash
+      t.timestamps null :false
     end
-
     add_index :users, :email, unique: true
   end
 end

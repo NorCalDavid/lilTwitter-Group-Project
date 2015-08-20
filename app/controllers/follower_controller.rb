9f3c3da06373e8b6b3337a9320df09560431f639
David <mail@daviduli.com>
2015-08-20 16:17:25 -0700

    Updated Tweet and Follower Controllers

--- app/controllers/follower_controller.rb
+++ app/controllers/follower_controller.rb
@@ -1,3 +1,9 @@
 ###########################################
 ### Follower Controller
 ###########################################
+
+
+get '/test' do
+  @user = auth_current_user
+  erb :'relationships/_stats'
+end

843541d717daf483ebc363ecf21377d1aaab3e43
Kevin Yan <k3vinyan@gmail.com>
2015-08-21 00:13:04 -0700

    add slogan to homepage

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,29 +9,30 @@
 # end
 
 # Users
-users = []
+User.create(handle: "kevin", password_hash: "1234")
+# users = []
 
-User.create!(name: 'David Uli',
-             email: 'mail@daviduli.com',
-             handle: '#NorCalDavid',
-             password_hash: ["password_hash", "$2a$10$9dNZbib6VQTXA7xPvoewMOnwHpPZer0ZnSxDQqcKdJx1sk6Pz.gA."])
+# User.create!(name: 'David Uli',
+#              email: 'mail@daviduli.com',
+#              handle: '#NorCalDavid',
+#              password_hash: ["password_hash", "$2a$10$9dNZbib6VQTXA7xPvoewMOnwHpPZer0ZnSxDQqcKdJx1sk6Pz.gA."])
 
-User.create!(name: 'James Bomotti',
-             email: 'test@thisserver.com',
-             handle: '#JBomotti',
-             password_hash: ["password_hash", "$2a$10$S0fnTfu3cLc1G7V5dMI/8uAzeLnxM5I5diiKT4T3EUMnAKpUcJICK"])
+# User.create!(name: 'James Bomotti',
+#              email: 'test@thisserver.com',
+#              handle: '#JBomotti',
+#              password_hash: ["password_hash", "$2a$10$S0fnTfu3cLc1G7V5dMI/8uAzeLnxM5I5diiKT4T3EUMnAKpUcJICK"])
 
-# Tweets
+# # Tweets
 
-5.times do
-  content = Tweet.create( message: Faker::Lorem.characters(rand(105)+25), user_id: rand(2)+1 )
-end
+# 5.times do
+#   content = Tweet.create( message: Faker::Lorem.characters(rand(105)+25), user_id: rand(2)+1 )
+# end
 
-# Following relationships
-users = User.all
-user  = users.first
-following = users[0]
-followers = users[1]
-following.each { |followed| user.follow(followed) }
-followers.each { |follower| follower.follow(user) }
+# # Following relationships
+# users = User.all
+# user  = users.first
+# following = users[0]
+# followers = users[1]
+# following.each { |followed| user.follow(followed) }
+# followers.each { |follower| follower.follow(user) }
 

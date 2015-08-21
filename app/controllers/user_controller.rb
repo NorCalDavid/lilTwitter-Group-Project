b71071b7f25f1efcb4f2c13a3c3172f81e46abb0
Ben & brenda_nguyen <pair+benjamh.brenguyen711@devbootcamp.com>
2015-08-21 13:41:11 -0700

    add login and logout features

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -16,7 +16,7 @@ get '/users/new' do
 post '/users/new' do
   @user = User.new
   @user.name = params[:name]
-  @user.password = params[:password]
+  @user.password = params[:password_hash]
 
   p "*"*100
 p params
@@ -30,7 +30,31 @@ p params
   end
 end
 
+# once you're in
 get '/users/:id' do
   @user = User.find(params[:id])
   erb :'users/show'
+end
+
+# find this user, check password, and login (redirect)
+
+post '/users' do
+  #if user input = password in db
+    @name = params[:name]
+  @user = User.find_by(name: @name)
+
+  if(@user && @user.password == params[:password_hash])
+    auth_login(@user)
+    # flash[:message] = 'Thank you for logging in'
+    redirect "/users/#{@user.id}"
+  else
+    # @form_error = 'Unable to log you in'
+    redirect '/'
+  end
+end
+
+
+get '/logout' do
+  auth_logout
+  redirect '/'
 end
